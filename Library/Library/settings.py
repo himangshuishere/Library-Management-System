c541091fd1ada151c6df31b308d1ae0f0bd2ccfb
himangshuishere <dehimangshu2020@outlook.com>
2022-02-27 20:03:40 +0530

    Added local tunneling support  and changed allowed hosts in settings.py

--- Library/Library/settings.py
+++ Library/Library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&$mx81@)v9rk2j+-)hu9i)4=yk#fsbn2skx+43x60f!$k4(9(#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
